350303872fc8c95e122b91f93b56efe0f207bb80
Alexander Verevkin <89090823215@yandex.ru>
2026-01-17 17:50:29 +0500

    Update low_entropy_protocol.h

--- lep/low_entropy_protocol.h
+++ lep/low_entropy_protocol.h
@@ -96,6 +96,8 @@ struct lep_v0_encoder_state
 	std::uint32_t g_seed = 0;
 
 	static constexpr uint8_t HEADER = 0b00110001;
+	//				    ^^  ^^^^
+	//			       version  "hash"
 
 	constexpr std::uint32_t fastrand()
 	{
@@ -115,9 +117,6 @@ constexpr std::vector<uint8_t> put_lep_v0(lep_v0_encoder_state &state, const uin
 	encoded_data.reserve(size * 3 + header_size);
 
 	encoded_data[0] = lep_v0_encoder_state::HEADER;
-	//		    ^^  ^^^^
-	//		version	 "hash"
-
 	encoded_data[1] = 0b00100001;
 	//		      ^
 	//		burst bit;	everything else is "payload type"; 33 ~ MP2T
